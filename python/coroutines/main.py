1d980862ab35bbd5a2928dae3ec9580ef833a045
Ivo Donchev <i.donchev@hacksoft.io>
2022-01-28 14:24:15 +0200

    Add coroutines example

---
+++ python/coroutines/main.py
@@ -0,0 +1,28 @@
+from sys import argv
+import asyncio
+
+
+async def func():
+    print("Async")
+    await asyncio.sleep(1)
+    print("Django")
+
+
+async def main(*, coroutines_count):
+    print(f'Running {coroutines_count} coroutines...')
+    await asyncio.gather(*[func() for i in range(int(coroutines_count))])
+    print('Done!')
+
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print(
+            '''
+            ERROR:
+            You must provide the number of coroutines!
+            Example: `python main.py 3` will use 3 coroutines
+            '''
+        )
+    else:
+        coroutines_count = argv[1]
+        asyncio.run(main(coroutines_count=argv[1]))
